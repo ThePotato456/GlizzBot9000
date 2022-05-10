b73cb303f72a1a3fb1ec9b0dadae0689465561d7
ThePotato456 <ThePotato456>
2022-05-10 00:24:46 +0000

    changed the way cogs are loaded

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ class Bot(commands.Bot):
     @commands.Cog.listener()
     async def on_ready(self):
         print('[BOT] Logged in as {0} ({0.id})'.format(self.bot.user))
-        print('[!] Interact with it using the prefix \'{0}\''.format(self.bot.command_prefix))
-        print('[!] Invite to your server with this link \n{0}'.format('https://discord.com/api/oauth2/authorize?client_id=972616556347527258&permissions=8&scope=bot'))
+        print('[BOT] Interact with it using the prefix \'{0}\''.format(self.bot.command_prefix))
+        print('[BOT] Invite to your server with this link \n{0}'.format('https://discord.com/api/oauth2/authorize?client_id=972616556347527258&permissions=8&scope=bot'))
 
     def initialize_cogs(self):
         for file in os.listdir('./cogs'):
             if file.endswith(".py"):
                 name = file[:-3]
-                print(f"[*] Loading cog {name}")
+                print(f"[*] Loading cogs.{name}")
                 self.bot.load_extension(f"cogs.{name}")
 
     def run(self):
